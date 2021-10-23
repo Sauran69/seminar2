c21b1dc4315d6e26b95f8a70c94d6ff7f5ef9dac
Sauran69 <sako301101@gmail.com>
2021-10-23 11:49:13 -0400

    zahacha2

---
+++ exercise2.py
@@ -0,0 +1,12 @@
+print("Введите первое число: ")
+a = float(input()) * 2 
+
+print("Введите второе число: ")
+b = float(input()) / 3
+
+print("Введите третье число: ")
+c = float(input())
+
+
+print(a + b + c**c)
+
